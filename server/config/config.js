3be2e186d33e326988c6a8b783569bbbc7c5e9a4
cristonal araneda <cristobal.a85@gmail.com>
2019-05-30 16:05:50 -0400

    RestServet - Usuario listo para producion en  cloud

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,22 @@
 //===========================
 
 process.env.PORT = process.env.PORT || 3000;
+
+//===========================
+// ENTORNO
+//===========================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//===========================
+// DATA BASE
+//===========================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = process.env.MONGO_URL;
+}
+
+process.env.URLDB = urlDB;
--- server/serve.js
+++ server/serve.js
@@ -12,13 +12,14 @@ app.use(bodyParser.json())
 
 app.use((require('./routes/usuario')));
 
-mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true }, (err, res) => {
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true },
+    (err, res) => {
 
-    if (err) throw err;
+        if (err) throw err;
 
-    console.log('base de datos ONLINE');
+        console.log('base de datos ONLINE');
 
-});
+    });
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando por', process.env.PORT);
